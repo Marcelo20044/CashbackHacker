51f87569fc41a742bb6f760587411a680bb637e6
Khvostenko <khmark04@mail.ru>
2022-01-10 20:27:01 +0700

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void ShouldRemain() {
+
+        int actual= service.remain(201);
+        int expected = 799;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldRemainOverBoundary() {
+
+        int actual= service.remain(2011);
+        int expected = 989;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void ShouldRemainIsBoundary() {
+
+        int actual= service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
